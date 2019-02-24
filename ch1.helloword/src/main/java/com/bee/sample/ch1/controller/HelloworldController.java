503473d26abfe24a8aa714f44fe7dc315f5f5cb6
shangzhuzi <spole_168@qq.com>
2019-02-24 20:51:38 +0800

    init

---
+++ ch1.helloword/src/main/java/com/bee/sample/ch1/controller/HelloworldController.java
@@ -0,0 +1,26 @@
+package com.bee.sample.ch1.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.bee.sample.ch1.annotation.Function;
+/**
+ * 访问:http://127.0.0.1:18080/sayhello.html?name=springboot
+ * @author xiandafu
+ *
+ */
+@Controller
+public class HelloworldController {
+	
+	
+	@RequestMapping("/sayhello.html")
+	@Function()
+	public @ResponseBody String say(String name){
+		String currentDTStr = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+		return "hello "+ currentDTStr+" name = " +name;
+	}
+}
