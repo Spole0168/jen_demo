07a36bd6c817a709d99fd8bf0bad7e44a6778feb
shangzhuzi <spole_168@qq.com>
2019-03-03 19:00:58 +0800

    1233

--- helloworld/src/main/java/com/bee/sample/ch1/controller/HelloworldController.java
+++ helloworld/src/main/java/com/bee/sample/ch1/controller/HelloworldController.java
@@ -8,19 +8,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bee.sample.ch1.annotation.Function;
-/**
- * 访问:http://127.0.0.1:18080/sayhello.html?name=springboot
- * @author xiandafu
- *
- */
+
 @Controller
 public class HelloworldController {
-	
-	
+
+	/**
+	 * 访问:http://127.0.0.1:18080/sayhello.html?name=springboot
+	 * @author xiandafu
+	 *
+	 */
 	@RequestMapping("/sayhello.html")
 	@Function()
 	public @ResponseBody String say(String name){
 		String currentDTStr = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
 		return "99999----hello "+ currentDTStr+"\t \n\n SSSSSSS \n name = " +name;
+	}
+	/**
+	 * 访问:http://127.0.0.1:18080/testddd?name=springboot
+	 * @author xiandafu
+	 *
+	 */
+	@RequestMapping("/testddd")
+	public @ResponseBody String testddd(String name){
+		String currentDTStr = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+		return "testddd "+ currentDTStr+"\t \n\n SSSSSSS \n name = " +name;
 	}
 }
